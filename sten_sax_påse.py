cdb178f68802dc21fb2fd400b83a1721cd5f2700
Theo Larefalk <theo.larefalk@learnet.se>
2019-10-15 10:20:18 +0200

    seqvence 3 done

--- sten_sax_påse.py
+++ sten_sax_påse.py
@@ -1,36 +1,19 @@
 import random
 
-sten = sten  
-sax = sax
-pase = pase 
+val1 = 0
+val2 = 0
+runda = 1
+loop = True
+point1 = 0
+point2 = 0
+pc = 0
 
 
-#if pase + sax = förlust
-#if sax + pase = vinst 
-#if sten + sax = vinst
-#if sten + pase = förlust
-#if sax + sten = förlust
-#if pase + sten = vinst
+while loop: 
+    if input("Vill du köra?: y/N") == "y":
+        pc = input("Vem vill du spela mot?: ")    
+        if input("")
+    
 
+    
 
-menu = 0
-while menu != 2:
-    if menu == 1:
-    print("Ditt saldo är: " + str(saldo) + "kr")
-    menu = (input("Vill du spela sten_sax_pase?: Ja/Nej")
-    if menu == 0: 
-        float(input("Gör en insättning: "))
-    elif menu == 2:
-        saldo = saldo - float(input("Gör ett uttag: "))
-    else:
-        print("Fel eller avslut")
-
-
-menu = 0
-while menu != 0:
-    input("Vill du spela sten, sax, påse?: Y/N")
-    if menu == y: 
-    input("vem vill du spela mot Player/PC: ")
-    if 
-    else:
-        print("Fel eller avslut")
