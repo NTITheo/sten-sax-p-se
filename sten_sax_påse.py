6000f438f11b62be1ce668b21329ff2cfef9f187
Theo Larefalk <theo.larefalk@learnet.se>
2019-10-15 10:52:23 +0200

    Seqvence 4 W.I.P

--- sten_sax_påse.py
+++ sten_sax_påse.py
@@ -11,9 +11,40 @@ pc = 0
 
 while loop: 
     if input("Vill du köra?: y/N") == "y":
-        pc = input("Vem vill du spela mot?: ")    
-        if input("")
-    
+        pc = input("Vem vill du spela mot?: pc/player")    
+        #spelare 1
+         val1 = input("Välj din hand: sten, sax, påse")
+            if val1 == "sten": 
+                val1 = 1
+            elif val1 == "sax":
+                val1 = 2
+            else val1 == "påse":
+                val1 = 3        
+        #spelare
+        if pc == "pc":  
+            val1 = random.randint(1,3)
+            else:
+            val2 = input("Välj din hand: sten, sax, påse")
+            if val2 == "sten": 
+                val2 = 1
+            elif val2 == "sax":
+                val2 = 2
+            else val2 == "påse":
+                val2 = 3
+            if val1 == val2:
+                pass
+            elif val1 == 1 and val2 == 2:
+                point1 += 1
+            elif val1 == 2 and val2 == 3:
+                point1 += 1
+            elif val1 == 3 and val2 == 1:
+                point1 += 1
+            else:
+                point2 += 1
+   
+
+
+
 
     
 
