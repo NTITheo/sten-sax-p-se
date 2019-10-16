d3a9490de1071acf30101e7d440e078cd63c1dbb
Theo Larefalk <theo.larefalk@learnet.se>
2019-10-16 13:22:51 +0200

    Seqvence 4 W.I.P HotFix

--- sten_sax_påse.py
+++ sten_sax_påse.py
@@ -14,14 +14,14 @@ while loop:
     if input("Vill du köra?: y/N") == "y":
         pc = input("Vem vill du spela mot?: pc/player") 
         while runda < 4:   
-            #spelare 1 val av hand
-                val1 = input("Välj din hand: sten, sax, påse")
-                if val1 == "sten": 
-                    val1 = 1
-                elif val1 == "sax":
-                    val1 = 2
-                else:
-                    val1 = 3
+        #spelare 1 val av hand
+            val1 = input("Välj din hand: sten, sax, påse")
+            if val1 == "sten": 
+                val1 = 1
+            elif val1 == "sax":
+                val1 = 2
+            else:
+                val1 = 3
 
             #spelare 2 val av hand
             if pc == "pc":
@@ -33,26 +33,26 @@ while loop:
                     val2 = 1
                 elif val2 == "sax":
                     val2 = 2
-                else val2 == "påse":
+                else:
                     val2 = 3
 
-                if val1 == val2:
-                    pass 
-                    print("Det blev lika")
-                elif val1 == 1 and val2 == 2:
-                    point1 += 1
-                    print("poäng till spelare 1")
-                elif val1 == 2 and val2 == 3:
-                    point1 += 1
-                    print("poäng till spelare 1")
-                elif val1 == 3 and val2 == 1:
-                    point1 += 1
-                    print("poäng till spelare 1")
-                else:
-                    point2 += 1
-                    print("poäng till spelare 2")
-                runda += 1
-                print("Nästa runda. Poängen är: " + str(point1) + "-" + str(point2))
+            if val1 == val2:
+                pass 
+                print("Det blev lika")
+            elif val1 == 1 and val2 == 2:
+                point1 += 1
+                print("poäng till spelare 1")
+            elif val1 == 2 and val2 == 3:
+                point1 += 1
+                print("poäng till spelare 1")
+            elif val1 == 3 and val2 == 1:
+                point1 += 1
+                print("poäng till spelare 1")
+            else:
+                point2 += 1
+                print("poäng till spelare 2")
+            runda += 1
+            print("Nästa runda. Poängen är: " + str(point1) + "-" + str(point2))
 
 
 
